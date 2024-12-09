3141ff75ef7fdd466139ee41288cf9800a024b4a
beingvin <beingvini@gmail.com>
2024-12-09 21:13:05 +0530

    add bootsrat 4.0.0, create header menu , footer menu and register into function.php

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -2,7 +2,17 @@
 
 function load_scripts()
 {
-    wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0');
+    wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all');
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js', array('jquery'), '4.0.0', true);
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css', array(), '4.0.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'load_scripts');
+
+register_nav_menus(
+    array(
+        'my_main_menu' => 'Main Menu',
+        'footer_menu' => 'Footer Menu'
+    )
+
+);
--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -11,11 +11,24 @@
 <body <?php body_class(); ?>>
     <header>
         <section class="top-bar">
-            <div class="social-media-icons">Icons</div>
-            <div class="search">Search</div>
+            <div class="container">
+                <div class="row">
+                    <div class="social-media-icons col-xl-10 col-md-8 col-sm-8 col-6">Icons</div>
+                    <div class="search col-xl-2 col-md-4 col-sm-4 col-6 text-right">Search</div>
+                </div>
+            </div>
+
         </section>
         <section class="menu-area">
-            <section class="logo">Logo</section>
-            <nav class="main-menu">Menu</nav>
+            <div class="container">
+                <div class="align">
+                    <div class="row">
+                        <section class="logo col-md-2 col-sm-12 col-12 text-center">Logo</section>
+                        <nav class="main-menu col-md-10 text-right">
+                            <?php wp_nav_menu(array('theme_location' => 'my_main_menu')) ?>
+                        </nav>
+                    </div>
+                </div>
+            </div>
         </section>
     </header>

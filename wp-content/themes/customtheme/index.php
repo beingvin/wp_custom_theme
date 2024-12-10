94d4a204527cc35ba973ac467bfcb0ef7503d68d
beingvin <beingvini@gmail.com>
2024-12-10 21:25:35 +0530

    learn about php loops and create blog page, home page

--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -1,13 +1,59 @@
 <?php get_header() ?>
 <div class="content-area">
     <main>
-        <section class="slide">Slide</section>
-        <section class="services">Services</section>
-        <section class="middle-area">
-            <aside class="sidebar">Sidebar</aside>
-            <div class="news">News</div>
+        <section class="slide">
+            <div class="container">
+                <div class="row text-center">
+                    Slide
+                </div>
+            </div>
         </section>
-        <section class="map">Map</section>
+        <section class="services">
+            <div class="container">
+                <div class="row">
+                    Services
+                </div>
+            </div>
+        </section>
+        <section class="middle-area">
+            <div class="container">
+                <div class="row ">
+                    <aside class="sidebar col-md-3">Sidebar</aside>
+                    <div class="news col-md-9">
+                        <?php
+                        //    if there is any post
+                        if (have_posts()):
+                            // While have posts, show them to us 
+                            while (have_posts()): the_post();
+                        ?>
+                                <article>
+                                    <h2><?php the_title() ?></h2>
+                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                    <p>Categories: <?php the_category(' '); ?> </p>
+                                    <p><?php the_tags('Tags: ', ', '); ?></p>
+                                    <p><?php the_content(); ?></p>
+
+                                </article>
+                            <?php
+                            endwhile;
+                        else:
+                            ?>
+                            <p>There's nothing yet to be displayed</p>
+                        <?php
+                        endif;
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="map">
+            <div class="container">
+                <div class="row text-center">
+                    Map
+                </div>
+            </div>
+        </section>
     </main>
 </div>
 <?php get_footer() ?>

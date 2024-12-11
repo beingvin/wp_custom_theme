3af1dd6238be65dd4bac78d7a29aa8a34d8fa814
beingvin <beingvini@gmail.com>
2024-12-11 20:42:57 +0530

    create content.php and use get_template_part()

--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -28,24 +28,10 @@
                         if (have_posts()):
                             // While have posts, show them to us 
                             while (have_posts()): the_post();
-                        ?>
-                                <article>
-                                    <h2><?php the_title() ?></h2>
-                                    <?php 
-                                    the_post_thumbnail('thumbnail');  
-                                    // the_post_thumbnail((array(375, 275)));  
-                                    
-                                    ?>
-                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                                    <p>Categories: <?php the_category(' '); ?> </p>
-                                    <p><?php the_tags('Tags: ', ', '); ?></p>
-                                    <p><?php the_content(); ?></p>
-
-                                </article>
-                            <?php
-                            endwhile;
+                                get_template_part('template-parts/content');
+                        endwhile;
                         else:
-                            ?>
+                        ?>
                             <p>There's nothing yet to be displayed</p>
                         <?php
                         endif;
---
+++ wp-content/themes/customtheme/template-parts/content.php
@@ -0,0 +1,13 @@
+<article>
+    <h2><?php the_title() ?></h2>
+    <?php
+    the_post_thumbnail('thumbnail');
+    // the_post_thumbnail((array(375, 275)));  
+
+    ?>
+    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+    <p>Categories: <?php the_category(' '); ?> </p>
+    <p><?php the_tags('Tags: ', ', '); ?></p>
+    <p><?php the_content(); ?></p>
+
+</article>

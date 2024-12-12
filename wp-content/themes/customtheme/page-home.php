4e577464c3cf92e980b7d511b78f64cc85f505e3
beingvin <beingvini@gmail.com>
2024-12-13 03:37:56 +0530

    install slider plugin and add slider to home page

--- wp-content/themes/customtheme/page-home.php
+++ wp-content/themes/customtheme/page-home.php
@@ -3,11 +3,9 @@
 <div class="content-area">
     <main>
         <section class="slide">
-            <div class="container">
-                <div class="row text-center">
-                    Slide
-                </div>
-            </div>
+            <?php
+            echo do_shortcode('[smartslider3 alias="home-slider"]');
+            ?>
         </section>
         <section class="services">
             <div class="container p-5">
@@ -48,49 +46,49 @@
                 <div class="row ">
                     <aside class="sidebar col-md-3 h-100 py-5"><?php get_sidebar('home'); ?></aside>
                     <div class="news col-md-9">
-    <div class="row">
-        <?php
-        // First Loop
-        $featured = new WP_Query('post_type=post&posts_per_page=1&cat=22,17');
-        if ($featured->have_posts()):
-            while ($featured->have_posts()): $featured->the_post();
-        ?>
-                <div class="col-12">
-                    <?php get_template_part('template-parts/content', 'featured') ?>
-                </div>
-            <?php
-            endwhile;
-            wp_reset_postdata();
-        endif;
+                        <div class="row">
+                            <?php
+                            // First Loop
+                            $featured = new WP_Query('post_type=post&posts_per_page=1&cat=22,17');
+                            if ($featured->have_posts()):
+                                while ($featured->have_posts()): $featured->the_post();
+                            ?>
+                                    <div class="col-12">
+                                        <?php get_template_part('template-parts/content', 'featured') ?>
+                                    </div>
+                                <?php
+                                endwhile;
+                                wp_reset_postdata();
+                            endif;
 
-        // Second Loop
-        $args = array(
-            'post_type' => 'post',
-            'posts_per_page' => 2,
-            'category__not_in' => array(17),
-            'category__in' => array(22),
-            'offset' => 1
-        );
-        $secondary = new WP_Query($args);
-        if ($secondary->have_posts()):
-            ?>
-            <div class="row">
-                <?php
-                while ($secondary->have_posts()): $secondary->the_post();
-                ?>
-                    <div class="col-sm-6 mb-3">
-                        <?php get_template_part('template-parts/content', 'secondary') ?>
+                            // Second Loop
+                            $args = array(
+                                'post_type' => 'post',
+                                'posts_per_page' => 2,
+                                'category__not_in' => array(17),
+                                'category__in' => array(22),
+                                'offset' => 1
+                            );
+                            $secondary = new WP_Query($args);
+                            if ($secondary->have_posts()):
+                                ?>
+                                <div class="row">
+                                    <?php
+                                    while ($secondary->have_posts()): $secondary->the_post();
+                                    ?>
+                                        <div class="col-sm-6 mb-3">
+                                            <?php get_template_part('template-parts/content', 'secondary') ?>
+                                        </div>
+                                    <?php
+                                    endwhile;
+                                    ?>
+                                </div>
+                            <?php
+                                wp_reset_postdata();
+                            endif;
+                            ?>
+                        </div>
                     </div>
-                <?php
-                endwhile;
-                ?>
-            </div>
-        <?php
-            wp_reset_postdata();
-        endif;
-        ?>
-    </div>
-</div>
 
                 </div>
             </div>

1c19039e8ca903043900347d4c91fd3ab92cd765
beingvin <beingvini@gmail.com>
2024-12-13 17:40:03 +0530

    update general-template.php and use contanct-form 7 to display forms

--- wp-content/themes/customtheme/general-template.php
+++ wp-content/themes/customtheme/general-template.php
@@ -13,13 +13,22 @@ Template Name: Genaral Template
 
         <section class="middle-area">
             <div class="container">
-                <div class="row ">
-                    <div class="news my-5">
-                        <h3>Page Title</h3></br>
-                        In auctor luctus sapien ut viverra. Nunc faucibus tempor congue. Fusce pretium, nisl quis iaculis semper, justo neque condimentum velit, non commodo odio dolor vitae lacus. Mauris ornare purus sit amet iaculis dictum. Proin et consequat ante. In pulvinar turpis ut enim vulputate, non accumsan lectus condimentum. Curabitur nec aliquet justo, a ullamcorper justo. Morbi viverra lorem tristique orci rhoncus, in bibendum elit iaculis. Duis et elit sed elit eleifend lobortis. Nulla ullamcorper, eros ut sollicitudin pellentesque, lorem dolor condimentum felis, sit amet mollis dolor ipsum nec lectus. Nullam at metus rhoncus, tincidunt erat sit amet, iaculis enim.
+                <div class="general-template">
+                    <?php
+                    //If there are any posts
+                    if (have_posts()):
+                        //while have posts, show them to us 
+                        while (have_posts()): the_post();
+                    ?>
+                            <article>
+                                <h2><?php the_title(); ?></h2>
+                                <p><?php the_content(); ?></p>
+                            </article>
 
-                        Etiam at gravida velit. Proin dictum fringilla nibh non vulputate. Suspendisse tellus ipsum, pharetra eget dapibus vitae, faucibus a arcu. Sed eleifend urna sit amet risus volutpat, at finibus orci sagittis. Phasellus sed pulvinar nulla. Sed convallis, dolor dignissim laoreet vulputate, dolor ipsum lobortis est, vel gravida nisi augue nec lacus. In tristique maximus orci, vel volutpat nibh accumsan quis. Etiam et malesuada mi. Curabitur at libero at diam efficitur elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut nulla et mi aliquet semper. Ut in porttitor leo. Ut et posuere ligula, nec luctus nulla.
-                    </div>
+                    <?php
+                        endwhile;
+                    endif;
+                    ?>
                 </div>
             </div>
         </section>

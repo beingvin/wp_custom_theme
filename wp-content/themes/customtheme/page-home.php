ebcc64a0caf5db1d6d6ab4e24a09d47d61deb19c
beingvin <beingvini@gmail.com>
2024-12-13 18:56:58 +0530

    add googlemap in homepage

--- wp-content/themes/customtheme/page-home.php
+++ wp-content/themes/customtheme/page-home.php
@@ -96,7 +96,7 @@
         <section class="map">
             <div class="container">
                 <div class="row text-center">
-                    Map
+                    <iframe class="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448193.9510303648!2d76.76356891374634!3d28.644287353477026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1734096087746!5m2!1sen!2sin" width="400" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </section>
